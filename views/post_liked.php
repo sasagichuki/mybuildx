a86b3d7041723d2cbd1aac8b01432d516a089e26
sasagichuki@gmail.com <sasagichuki@gmail.com>
2013-07-27 20:25:38 +0300

    Initial commit on like gate branch

---
+++ views/post_liked.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Post Like</title>
+
+  
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400|Armata' rel='stylesheet' type='text/css'>
+
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+	<div class="">
+		<div class="row">
+			<div class="large-12 columns banner">
+				<img src="img/main_banner.jpg" alt="" class="main-banner">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="section-container tabs auto " data-section>
+			  <section class="">
+			    <p class="title" data-section-title><a href="#panel1" class="tab-title tab1">Only 4499/-</a></p>
+			    <div class="content" data-section-content>
+			    	<img alt="" src="img/greatoffer.png" />
+			    	<h3>Great Special offer price</h3>
+			      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			    </div>
+			  </section>
+			  <section class="">
+			    <p class="title" data-section-title><a href="#panel2" class="tab-title">Free local TV</a></p>
+			    <div class="content" data-section-content>
+			    	<img alt="" src="img/local.png" />
+			    	<h3>Watch the best local tv free</h3>
+			      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			    </div>
+			  </section>
+			  <section class="">
+			    <p class="title" data-section-title><a href="#panel2" class="tab-title">Go Digital now</a></p>
+			    <div class="content tab-content" data-section-content>
+					<img alt="" src="img/godigital.png" />
+					<h3>Go digital with GOtv</h3>
+			      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			    </div>
+			  </section>
+			</div>	
+		</div>
+		
+		<div class="row">
+			<div class="footer"></div>
+		</div>
+	</div>
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
+  
+  <script src="js/foundation.min.js"></script>
+  <!--
+  
+  <script src="js/foundation/foundation.js"></script>
+  
+  <script src="js/foundation/foundation.alerts.js"></script>
+  
+  <script src="js/foundation/foundation.clearing.js"></script>
+  
+  <script src="js/foundation/foundation.cookie.js"></script>
+  
+  <script src="js/foundation/foundation.dropdown.js"></script>
+  
+  <script src="js/foundation/foundation.forms.js"></script>
+  
+  <script src="js/foundation/foundation.joyride.js"></script>
+  
+  <script src="js/foundation/foundation.magellan.js"></script>
+  
+  <script src="js/foundation/foundation.orbit.js"></script>
+  
+  <script src="js/foundation/foundation.reveal.js"></script>
+  
+  <script src="js/foundation/foundation.section.js"></script>
+  
+  <script src="js/foundation/foundation.tooltips.js"></script>
+  
+  <script src="js/foundation/foundation.topbar.js"></script>
+  
+  <script src="js/foundation/foundation.interchange.js"></script>
+  
+  <script src="js/foundation/foundation.placeholder.js"></script>
+  
+  <script src="js/foundation/foundation.abide.js"></script>
+  
+  -->
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
